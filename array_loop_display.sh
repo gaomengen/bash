c0a38efb9a0d487cf39cd08b34caa053fe6a5020
gaomengen <gaomengen@gmail.com>
2019-03-12 14:51:28 -0400

    array, for loop

--- array_loop_display.sh
+++ array_loop_display.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # simple array list and loop for display
-SERVERLIST=("websrv01" "websrv02" "websrv03" "websrv04")
+SERVERLIST=("websrv01" "websrv02" "websrv03" "websrv04" "webserver05")
 COUNT=0
 for INDEX in ${SERVERLIST[@]}; do
 	echo "Processing Server: ${SERVERLIST[COUNT]}"
